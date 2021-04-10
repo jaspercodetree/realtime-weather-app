884b13d4294b7c39cccf7d67a7ab4301250ae78a
Jasper <hometree23@gmail.com>
2021-04-10 15:47:40 +0800

    set currentWeather Data (static) and replace it in jsx

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
+// 引入React才能使用component  引入useState來使用hooks
 import React, { useState } from 'react';
+
+// 引入emotion套件
 import styled from '@emotion/styled';
+// 引入ThemeProvider
 import { ThemeProvider } from '@emotion/react';
+
+// 透過react建立時提供的元件名稱ReactComponent  逐一將要引入的svg檔案改成component  並修改成想要的名字 ex:DayCloudyIcon
 import { ReactComponent as DayCloudyIcon } from './images/day-cloudy.svg';
 import { ReactComponent as AirFlowIcon } from './images/airFlow.svg';
 import { ReactComponent as RainIcon } from './images/rain.svg';
 import { ReactComponent as RefreshIcon } from './images/refresh.svg';
 
-// import './App.css';
+//引入dayjs解決Safari 不支援 new Date('2021-12-12 10:10:00') 字串的問題
+import dayjs from 'dayjs';
 
+// 透過emotion把JSX改成component, 此處都是div
 const Container = styled.div`
   background-color: ${({ theme }) => theme.backgroundColor};
   height: 100%;
@@ -25,6 +33,7 @@ const WeatherCard = styled.div`
   padding: 30px 15px;
 `;
 
+// 可以透過觀察props 看到從父層傳來的 theme資料
 const Location = styled.div`
   ${(props) => console.log(props)}
   font-size: 28px;
@@ -55,10 +64,6 @@ const Temperature = styled.div`
 const Celsius = styled.div`
   font-weight: normal;
   font-size: 42px;
-`;
-
-const DayCloudy = styled(DayCloudyIcon)`
-  flex-basis: 30%;
 `;
 
 const AirFlow = styled.div`
@@ -104,6 +109,11 @@ const Refresh = styled.div`
   }
 `;
 
+// 將原本存在的svg  新增樣式 DayCloudy
+const DayCloudy = styled(DayCloudyIcon)`
+  flex-basis: 30%;
+`;
+
 const theme = {
   light: {
     backgroundColor: '#ededed',
@@ -125,30 +135,51 @@ const theme = {
 };
 
 const App = () => {
+  //設定可變動的主題樣式
   const [currentTheme, setCurrentTheme] = useState('light');
 
+  //設定可變動的資料
+  const [currentWeather, setCurrentWeather] = useState({
+    locationName: '臺北市',
+    description: '多雲時晴',
+    windSpeed: 1.1,
+    temperature: 22.9,
+    rainPossibility: 48.3,
+    observationTime: '2020-12-12 22:10:00',
+  });
+
   return (
+    // 透過ThemeProvider 可以將 theme=theme.dark 放進其包住的每一個component
+    // 如同 <Container theme=theme.dark> <WeatherCard theme=theme.dark>...每一個都建好 可傳值props給子層
     <ThemeProvider theme={theme[currentTheme]}>
       <Container>
         <WeatherCard>
-          <Location>台北市</Location>
-          <Description>多雲時晴</Description>
+          <Location>{currentWeather.locationName}</Location>
+          <Description>{currentWeather.description}</Description>
           <CurrentWeather>
             <Temperature>
-              23<Celsius>°C</Celsius>
+              {currentWeather.temperature}
+              <Celsius>°C</Celsius>
             </Temperature>
             <DayCloudy />
           </CurrentWeather>
           <AirFlow>
             <AirFlowIcon />
-            23 m/h
+            {currentWeather.windSpeed} m/h
           </AirFlow>
           <Rain>
             <RainIcon />
-            48%
+            {currentWeather.rainPossibility} %
           </Rain>
           <Refresh>
-            最後觀測時間：上午 12:03
+            {/* 我們只想顯示小時與分鐘  因此做以下改寫  1.利用intl換成台灣顯示 2.並且使用dayjs修復safari無法顯示字串時間的問題 */}
+            {/* 原先寫法 */}
+            {/* 最後觀測時間：{currentWeather.observationTime} */}
+            最後觀測時間：
+            {new Intl.DateTimeFormat('zh-TW', {
+              hour: 'numeric',
+              minute: 'numeric',
+            }).format(dayjs(currentWeather.observationTime))}
             <RefreshIcon />
           </Refresh>
         </WeatherCard>
